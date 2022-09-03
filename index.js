cfb40ec77ea208fd2a7413c4416f527c19c428ac
thomasrcham <guseldorph@gmail.com>
2022-09-03 14:41:45 -0500

    can't get the test to pass but everything works as expected

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+let newEntry = {
+    name: 'Jason',
+    email: 'jason@email.com',
+}
+
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(entry),
+//     };
+
+function submitData (entry) {
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(entry),
+        })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch( () => console.log('error'))
+}
+
+submitData(newEntry);
